2e0be5c249d0b8fbd23e6883b13cc4a177874391
s3409304 <s3409304@TCSPC0J9HWE.bns.bns>
2017-03-27 14:35:37 -0400

    Commit3

--- src/TestGitHub/HelloGitHub.java
+++ src/TestGitHub/HelloGitHub.java
@@ -5,6 +5,7 @@ public class HelloGitHub {
 	public static void main(String[] args) {
 		System.out.println("Hello From GitHub");
 		System.out.println("Hello From GitHub2");
+		System.out.println("Hello From GitHub3");
 
 	}
 
